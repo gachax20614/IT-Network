f3a9f733aac43bd12f05ca8559dfd4a68d93de42
gachax20614 <webprogrammer.ogata@gmail.com>
2022-01-30 23:39:11 +0900

    Aboutページの追加

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
-
+// ハンバーガーメニューのクラス付加・削除
+// コンテンツの選択時や違う場所を選択した際にメニューを非表示にする
   const nav = document.querySelector(".nav");
   document.addEventListener("click",(e) => {
     if(e.target.closest(".nav_toggle")) {
@@ -16,3 +17,16 @@
       nav_toggle.classList.remove("show");
     }
   });
+
+  jQuery(function(){
+    var windowWidth = $(window).width();
+    var headerHight = 40; 
+    jQuery('a[href^="#"]').click(function() {
+    var speed = 500;
+    var href= jQuery(this).attr("href");
+    var target = jQuery(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top-headerHight;
+    jQuery('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+   });
+});
