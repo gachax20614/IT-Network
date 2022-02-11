4ad1271a791f0b5a5e90ce617f7a42e075d7f831
gachax20614 <webprogrammer.ogata@gmail.com>
2022-02-11 17:34:52 +0900

    説明文の修正

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Hello-New world">
+    <meta name="description" content="このサイトは架空サイトです。">
     <title>IT Network</title>
     <link rel="shortcut icon" href="img/favicon.svg">
     <!--CSS-->
